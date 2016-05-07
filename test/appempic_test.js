184138dd44d8c3359937ef418f799e26bca8d558
mgochoa <mochoa1127@gmail.com>
2016-05-07 11:39:09 -0500

    test names

--- test/appempic_test.js
+++ test/appempic_test.js
@@ -5,11 +5,11 @@ var assert = require('should');
 
 describe('appempic', function () {
 
-  it('El nombre debe ser Manuel', function () {
-    appempic.getName().should.equal('Manuel');
+  it('El nombre debe ser Pedro', function () {
+    appempic.getName().should.equal('Pedro');
   });
-  it('El apellido debe ser Ochoa', function () {
-    appempic.getLastName().should.equal('Ochoa');
+  it('El apellido debe ser Perez', function () {
+    appempic.getLastName().should.equal('Perez');
   });
 
 
