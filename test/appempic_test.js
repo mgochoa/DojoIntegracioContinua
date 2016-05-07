03c60a366d3086b396fe781c50eba5d6a451b7aa
mgochoa <mochoa1127@gmail.com>
2016-05-07 11:04:35 -0500

    Resolve test fail

--- test/appempic_test.js
+++ test/appempic_test.js
@@ -6,7 +6,7 @@ var assert = require('should');
 describe('appempic', function () {
 
   it('El nombre debe ser Manuel', function () {
-    appempic.getName().should.equal('Manuel1');
+    appempic.getName().should.equal('Manuel');
   });
   it('El apellido debe ser Ochoa', function () {
     appempic.getLastName().should.equal('Ochoa');
